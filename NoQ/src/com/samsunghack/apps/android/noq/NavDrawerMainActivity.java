c434f052bb847c175e1343d4de5cd11aea6c1a6c
Somesh Kumar <somesh.k@mobispectra.com>
2013-08-17 20:58:21 -0700

    Organized the App and Added more features including location

--- NoQ/src/com/samsunghack/apps/android/noq/NavDrawerMainActivity.java
+++ NoQ/src/com/samsunghack/apps/android/noq/NavDrawerMainActivity.java
@@ -16,6 +16,7 @@
 
 package com.samsunghack.apps.android.noq;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 import android.app.Activity;
@@ -41,220 +42,282 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-/**
- * This example illustrates a common usage of the DrawerLayout widget
- * in the Android support library.
- * <p/>
- * <p>When a navigation (left) drawer is present, the host activity should detect presses of
- * the action bar's Up affordance as a signal to open and close the navigation drawer. The
- * ActionBarDrawerToggle facilitates this behavior.
- * Items within the drawer should fall into one of two categories:</p>
- * <p/>
- * <ul>
- * <li><strong>View switches</strong>. A view switch follows the same basic policies as
- * list or tab navigation in that a view switch does not create navigation history.
- * This pattern should only be used at the root activity of a task, leaving some form
- * of Up navigation active for activities further down the navigation hierarchy.</li>
- * <li><strong>Selective Up</strong>. The drawer allows the user to choose an alternate
- * parent for Up navigation. This allows a user to jump across an app's navigation
- * hierarchy at will. The application should treat this as it treats Up navigation from
- * a different task, replacing the current task stack using TaskStackBuilder or similar.
- * This is the only form of navigation drawer that should be used outside of the root
- * activity of a task.</li>
- * </ul>
- * <p/>
- * <p>Right side drawers should be used for actions, not navigation. This follows the pattern
- * established by the Action Bar that navigation should be to the left and actions to the right.
- * An action should be an operation performed on the current contents of the window,
- * for example enabling or disabling a data overlay on top of the current content.</p>
- */
 public class NavDrawerMainActivity extends Activity {
 	private static final String TAG = "NavDrawerMainActivity";
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
 
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    private String[] mPlanetTitles;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navdrawer_main);
+	// Navigation Menu items
+	private static final int NAV_MENU_HOME=0;
+	private static final int NAV_MENU_RESERVATIONS=1;
+	private static final int NAV_MENU_NEARBY=2;
+	private static final int NAV_MENU_PAYMENTS=3;
+	private static final int NAV_MENU_SETTINGS=4;
+	private static final int NAV_MENU_LENGTH=5;
 
-        mTitle = mDrawerTitle = getTitle();
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	private static String[] mAppFeatureTitles = new String[NAV_MENU_LENGTH];
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_navdrawer_main);
 
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		mTitle = mDrawerTitle = getTitle();
+		setupNavigationMenu();
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
+		// set a custom shadow that overlays the main content when the drawer
+		// opens
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+		// set up the drawer's list view with items and click listener
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, mAppFeatureTitles));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-                ) {
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
 
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description for accessibility */
+		R.string.drawer_close /* "close drawer" description for accessibility */
+		) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
 
-        if (savedInstanceState == null) {
-            selectItem(0);
-        }
-    }
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+		if (savedInstanceState == null) {
+			selectItemNew(0);
+		}
+	}
 
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-         // The action bar home/up action should open or close the drawer.
-         // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
-        switch(item.getItemId()) {
-        case R.id.action_websearch:
-            // create intent to perform web search for this planet
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-            // catch event that there's no activity to handle intent
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            } else {
-                Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-            }
-            return true;
-            
-        case R.id.action_reservations:
-            // create intent to perform web search for this planet
-            Intent reservationsIntent = new Intent(NavDrawerMainActivity.this, ReservationsActivity.class);
-            // intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-            // catch event that there's no activity to handle intent
-            startActivity(reservationsIntent);
-            return true;
-            
-        case R.id.action_reservation_form:
-			Intent calendarIntent = new Intent(NavDrawerMainActivity.this,
-					ReservationFormActivity.class);
-			// intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action buttons
+		switch (item.getItemId()) {
+		case R.id.action_websearch:
+			// create intent to perform web search for this planet
+			Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+			intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
 			// catch event that there's no activity to handle intent
-			startActivity(calendarIntent);
-            return true;
-               
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
+			if (intent.resolveActivity(getPackageManager()) != null) {
+				startActivity(intent);
+			} else {
+				Toast.makeText(this, R.string.app_not_available,
+						Toast.LENGTH_LONG).show();
+			}
+			return true;
 
-    /* The click listner for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    private void selectItem(int position) {
-        // update the main content by replacing fragments
-        Fragment fragment = new PlanetFragment();
-        Log.d(TAG,"+selectItem: positon = " + position);
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
+	/* The click listner for ListView in the navigation drawer */
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItemNew(position);
+		}
+	}
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+	private void selectItem(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = new PlanetFragment();
+		Log.d(TAG, "+selectItem: positon = " + position);
+		Bundle args = new Bundle();
+		args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+		fragment.setArguments(args);
 
-        // update selected item and title, then close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
 
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
+		// update selected item and title, then close the drawer
+		mDrawerList.setItemChecked(position, true);
+		setTitle(mAppFeatureTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+	
+	private void selectItemNew(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = new PlanetFragment();
+		Log.d(TAG, "+selectItem: positon = " + position);
+		Bundle args = new Bundle();
+		args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+		switch(position) {
+		case NAV_MENU_HOME:
+			Log.d(TAG,"Home Menu Pressed");
+			break;
+		case NAV_MENU_RESERVATIONS:
+			Log.d(TAG,"Reservations Menu Pressed");
+			Intent reservationsIntent = new Intent(NavDrawerMainActivity.this,
+					ReservationFormActivity.class);
+			startActivity(reservationsIntent);
+			break;
+			
+		case NAV_MENU_NEARBY:
+			Intent nearbyIntent = new Intent(NavDrawerMainActivity.this, NearbyActivity.class);
+			startActivity(nearbyIntent);
+			Log.d(TAG,"Nearby Menu Pressed");
+			break;
+		
+		case NAV_MENU_PAYMENTS:
+			Intent paymentsIntent = new Intent(NavDrawerMainActivity.this, PaymentsActivity.class);
+			startActivity(paymentsIntent);
+			Log.d(TAG,"Nearby Menu Pressed");
+			break;
+		
+		case NAV_MENU_SETTINGS:
+			Log.d(TAG,"Settings Menu Pressed");
+			break;
+			
+		}
+		fragment.setArguments(args);
 
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
+		// update selected item and title, then close the drawer
+		mDrawerList.setItemChecked(position, true);
+		setTitle(mAppFeatureTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+	
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
 
-    /**
-     * Fragment that appears in the "content_frame", shows a planet
-     */
-    public static class PlanetFragment extends Fragment {
-        public static final String ARG_PLANET_NUMBER = "planet_number";
+	/**
+	 * When using the ActionBarDrawerToggle, you must call it during
+	 * onPostCreate() and onConfigurationChanged()...
+	 */
 
-        public PlanetFragment() {
-            // Empty constructor required for fragment subclasses
-        }
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
-            int i = getArguments().getInt(ARG_PLANET_NUMBER);
-            String planet = getResources().getStringArray(R.array.planets_array)[i];
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 
-            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-                            "drawable", getActivity().getPackageName());
-            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
-            getActivity().setTitle(planet);
-            return rootView;
-        }
-    }
+	/**
+	 * Fragment that appears in the "content_frame", shows a planet
+	 */
+	public static class PlanetFragment extends Fragment {
+		public static final String ARG_PLANET_NUMBER = "planet_number";
+
+		public PlanetFragment() {
+			// Empty constructor required for fragment subclasses
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_planet,
+					container, false);
+			int i = getArguments().getInt(ARG_PLANET_NUMBER);
+			String planet = mAppFeatureTitles[i];
+
+			int imageId = getResources().getIdentifier(
+					planet.toLowerCase(Locale.getDefault()), "drawable",
+					getActivity().getPackageName());
+			((ImageView) rootView.findViewById(R.id.image))
+					.setImageResource(imageId);
+			getActivity().setTitle(planet);
+			return rootView;
+		}
+	}
+
+	// Sliding Menu items
+	class NavMenuItem {
+
+		int mItemId;
+		int mStringResourceId;
+		int mIconResourceId;
+
+		public NavMenuItem(int itemId, int stringResourceId, int iconResourceId) {
+			this.mItemId = itemId;
+			this.mStringResourceId = stringResourceId;
+			this.mIconResourceId = iconResourceId;
+			if(itemId < NAV_MENU_LENGTH) {
+				mAppFeatureTitles[itemId] = getResources().getString(stringResourceId);
+			}
+		}
+
+		public int getItemId() {
+			return mItemId;
+		}
+
+		public int getStringResourceId() {
+			return mStringResourceId;
+		}
+
+		public int getIconResourceId() {
+			return mIconResourceId;
+		}
+	}
+	
+	private void setupNavigationMenu() {
+		ArrayList<NavMenuItem> navMenuItemsList = new ArrayList<NavMenuItem>();
+		navMenuItemsList.add(new NavMenuItem(NAV_MENU_HOME, R.string.title_home, R.drawable.ic_launcher));
+		navMenuItemsList.add(new NavMenuItem(NAV_MENU_RESERVATIONS, R.string.title_reservations,R.drawable.ic_launcher));
+		navMenuItemsList.add(new NavMenuItem(NAV_MENU_NEARBY, R.string.title_nearby_restaurants,R.drawable.ic_launcher));
+		navMenuItemsList.add(new NavMenuItem(NAV_MENU_PAYMENTS, R.string.title_payments,R.drawable.ic_launcher));
+		navMenuItemsList.add(new NavMenuItem(NAV_MENU_SETTINGS, R.string.title_settings,R.drawable.ic_launcher));
+	}
 }
